878de2f1a18aa7b8dd53d9bfa09130ff0d0d78c5
Sara Ghara <saraghara89@gmail.com>
2026-08-19 16:20:44 +0300

    Protect hotel statistics endpoint

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { getStats } = require('../controllers/statsController');
+const { authenticate } = require('../middleware/auth');
+
 const router = express.Router();
-router.get('/', getStats);
+router.get('/', authenticate, getStats);
 module.exports = router;
