81bb3c18164b97cb69b026665172747ff029127e
Sara Ghara <saraghara89@gmail.com>
2026-08-19 16:20:38 +0300

    Protect room management endpoints

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { getRooms, updateRoom } = require('../controllers/roomController');
+const { authenticate, requireCsrf, requireRole } = require('../middleware/auth');
+
 const router = express.Router();
-router.get('/', getRooms);
-router.put('/:id', updateRoom);
+router.get('/', authenticate, getRooms);
+router.put('/:id', authenticate, requireCsrf, requireRole('owner', 'admin', 'staff'), updateRoom);
 module.exports = router;
